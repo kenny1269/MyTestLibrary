6e151ecfc4707f278914095835a5f5ff87790544
Kenny Yu <keyu@theknotww.com>
2022-08-18 17:28:24 +0800

    init commit

---
+++ Sources/Hello/Hello.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by keyu on 2022/8/18.
+//
+
+import Foundation
+
+
+//print("123")
+
+//static func main() {
+//    print("123")
+//}
+
+
+@main
+enum MyScript {
+  static func main() {
+    print("Hello Xcode")
+  }
+}
